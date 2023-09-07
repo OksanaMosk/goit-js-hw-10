40dddc7200d2fc0675042a0f37541273a83e9093
OksanaMosk <oks-m3@ukr.net>
2023-09-08 01:01:46 +0300

    2

--- src/index.js
+++ src/index.js
@@ -8,33 +8,54 @@ new SlimSelect({
   },
 });
 
-BASE_URL = `https://api.thecatapi.com/v1/breeds`;
+BASE_URL = `https://api.thecatapi.com/v1/`;
 axios.defaults.headers.common['x-api-key'] =
   'live_Z0UHJxttXDpi84D6XggncnWBSjuu0VNSoSROyArWqIofjKLv9bkMBKAH7wCjVcZ6';
 
 const refs = {
-  container: document.querySelector('.cat-info'),
+  containerCatInfo: document.querySelector('.cat-info'),
 };
 
-fetch('https://api.thecatapi.com/v1/images/0XYvRd7oD')
-  .then(response => {
-    return response.json();
-  })
-  .then(cat => {
-    breed_ids = cat.id;
-    const murkup = breed_ids;
-    console.log(cat);
-    refs.container.innerHTML = murkup;
-  })
-  .catch(error => {
-    console.log(error);
-  });
+fetchBreeds();
+function fetchBreeds() {
+  fetch('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      return response.json();
+    })
+    .then(fetchCatByBreed)
 
-const breedSelectEl = document.querySelector('#allowDeselect');
+    .catch(error => {
+      console.log(error);
+    });
+}
 
-const fetchBreeds = event => {
-  event.preventDefault();
-  console.log(event);
-};
+fetchCatByBreed();
+const breedId = e.currentTarget.value;
+function fetchCatByBreed(breedId) {
+   .then(data => {
+   const { url, breeds } = data[0];
+        return (refs.containerCatInfo.innerHTML = `<div class="cat"><img src = "${url} " alt = "${breeds[0].name}"></div>
+      <div><h2>${breeds[0].name}</h2><p>${breeds[0].description}</p><p><b>Temperament:</b>${breeds[0].temperament}</p></p</div>>`);
+   }
+  )
+  .catch(onFetchError);
+     } 
 
-breedSelectEl.addEventListener('select', fetchBreeds);
+// fetchBreeds().then(data => {
+//   data.forEach(element => {
+//     arrBreedsId.push({ text: element.name, value: element.id });
+//     console.log(arrBreedsId);
+//   });
+//   new SlimSelect({
+//     select: selector,
+//     data: arrBreedsId,
+//   });
+// });
+// const breedSelectEl = document.querySelector('#allowDeselect');
+
+// fetchBreeds = event => {
+//   event.preventDefault();
+//   console.log(event);
+// };
+
+// breedSelectEl.addEventListener('select', fetchBreeds);
